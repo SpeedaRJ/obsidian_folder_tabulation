9a736e9972df3bdf8a956d6da25f20f26a584073
SpeedaRJ <r.jutresa@gmail.com>
2024-10-05 15:10:06 +0200

    initial code commit

--- main.ts
+++ main.ts
@@ -1,134 +1,140 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { FileView, TextFileView, WorkspaceLeaf } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+import {
+	FolderTabulationSettings,
+	TabularFileConfiguration,
+} from "./src/definitions";
+import FolderTabulationHelper from "./src/helper";
+import FolderTabulationSettingsTab from "./src/settings";
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+const DEFAULT_SETTINGS: FolderTabulationSettings = {
+	newTab: "same",
+	hideButtons: false,
+	nextFileColor: "#b3b3b3",
+	prevFileColor: "#b3b3b3",
+};
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class FolderTabulation extends FolderTabulationHelper {
+	settings: FolderTabulationSettings;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	openViews = new Map<WorkspaceLeaf, TabularFileConfiguration>();
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "move-to-next-file-in-folder",
+			name: "Move to next file in folder",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
+				this.processTabulation(true, this.openViews, this.settings);
+			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+			id: "move-to-prev-file-in-folder",
+			name: "Move to previous file in folder",
+			callback: () => {
+				this.processTabulation(false, this.openViews, this.settings);
+			},
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+
+		this.addSettingTab(new FolderTabulationSettingsTab(this.app, this));
+
+		this.registerEvent(
+			this.app.workspace.on("active-leaf-change", () => {
+				const activeLeaf =
+					this.app.workspace.getActiveViewOfType(FileView);
+				if (!activeLeaf) return;
+
+				const leaf: WorkspaceLeaf = this.app.workspace.getLeaf(false);
+				if (!(leaf.view instanceof FileView)) return;
+
+				const [currentFileName, _parentFolderPath, currentFilePath]: [
+					string,
+					string,
+					string
+				] = this.getRelevantFileInformation();
+
+				if (!this.openViews.has(leaf)) {
+					const buttonNext = (leaf.view as FileView).addAction(
+						"arrow-right-to-line",
+						"Tab to next file",
+						() => {
+							(this.app as any).commands.executeCommandById(
+								"folder-tabulation:move-to-next-file-in-folder"
+							);
+						}
+					);
+					buttonNext.addClasses([
+						"folder-tabular-button",
+						"button-visible",
+					]);
+
+					const buttonPrev = (leaf.view as FileView).addAction(
+						"arrow-left-to-line",
+						"Tab to previous file",
+						() => {
+							(this.app as any).commands.executeCommandById(
+								"folder-tabulation:move-to-prev-file-in-folder"
+							);
+						}
+					);
+					buttonPrev.addClasses([
+						"folder-tabular-button",
+						"button-visible",
+					]);
+
+					this.openViews.set(leaf, {
+						path: currentFilePath,
+						buttons: new Map([
+							["button-next", buttonNext],
+							["button-prev", buttonPrev],
+						]),
+					});
+					
+					const unloadedViews = [...this.openViews.keys()].filter(
+						(leaf) => leaf.getViewState().type === "empty"
+					);
+					for (const view of unloadedViews) {
+						this.openViews.delete(view);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+				const buttons = this.getButtons(leaf, this.openViews);
+				if (buttons) {
+					const buttonNext: HTMLElement | undefined =
+						buttons.get("button-next");
+					const buttonPrev: HTMLElement | undefined =
+						buttons.get("button-prev");
+					if (buttonNext !== undefined && buttonPrev !== undefined) {
+						this.setButtonDisplay(
+							currentFileName,
+							buttonNext,
+							buttonPrev,
+							this.settings
+						);
+						this.colorButtons(
+							buttonNext,
+							buttonPrev,
+							this.settings
+						);
+					}
+				}
+			})
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		this.setSettings(this.settings, this.openViews);
 	}
 }
